dfb8b9b995c09ec0748c0b53a85eba3afa16b877
mardon <mar.don@seznam.cz>
2015-02-07 08:50:39 +0100

    2

---
+++ app/Controllers/loginController.php
@@ -0,0 +1,31 @@
+<?php
+
+class loginController extends Controller
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index () {
+        Session::set('authentication', true);
+        Session::set('level', 'basic');
+
+        Session::set('var1', 'var1');
+        Session::set('var2', 'var2');
+
+        $this->redirect('login/mortar');
+    }
+
+    public function mortar()
+    {
+        echo 'Level: '.Session::get('level') . '<br>';
+        echo 'Var1: '.Session::get('var1') . '<br>';
+        echo 'Var2: '.Session::get('var2') . '<br>';
+    }
+
+    public function logout()
+    {
+        Session::destroy();
+        $this->redirect('login/mortar');
+    }
+}
