5c23661e160b0a2d677a62bc404471ba9c323030
MamuSha <mar.don@seznam.cz>
2015-02-10 14:37:32 +0100

    login edited

--- app/Controllers/loginController.php
+++ app/Controllers/loginController.php
@@ -2,19 +2,55 @@
 
 class loginController extends Controller
 {
+    private $_login;
     public function __construct() {
         parent::__construct();
+        $this->_login = $this->loadModel('login');
     }
 
     public function index () {
 
-        $this->_view->tile = "Přihlašení";
+        $this->_view->title = "Přihlašení";
 
-        if ($this->getInt($enviar) == 1) {
-            $this->data = $_POST;
+        if ($this->getInt('enviar') == 1) {
+            $this->_view->data = $_POST;
+
+            if(!$this->getAlphaNum('user')) {
+                $this->_view->_error = 'Není zadáno správné jméno';
+                $this->_view->render('index', 'login');
+                exit;
+            }
+
+            if(!$this->getSql('pass')) {
+                $this->_view->_error = 'Není zadáno správné heslo';
+                $this->_view->render('index', 'login');
+                exit;
+            }
+
+            $row = $this->_login->getUser(
+                $this->getAlphaNum('user'),
+                $this->getSql('pass')
+            );
+
+            if(!$row) {
+                $this->_view->_error = 'Přihlášení se nezdařilo';
+                $this->_view->render('index', 'login');
+                exit;
+            }
+
+            if($row['state'] != 1 ) {
+                $this->_view->_error = 'Uživatel neaktivní';
+                $this->_view->render('index', 'login');
+                exit;
+            }
+
             Session::set('authentication', true);
-            Session::set('level', 'user');
+            Session::set('level', $row['role']);
+            Session::set('user', $row['user']);
+            Session::set('id_user', $row['id']);
             Session::set('time', time());
+
+            $this->redirect('');
         }
 
         $this->_view->render('index','login');
--- app/Models/loginModel.php
+++ app/Models/loginModel.php
@@ -8,7 +8,7 @@ class loginModel extends Model
 
     public function getUser($user, $password) {
         $data = $this->_db->query(
-            "select * from user" .
+            "select * from user " .
             "where user= '$user' " .
             "and pass = '" . md5($password) . "'"
             );
